6991bb47143ececbcdd12b96ed6376c3c1e8b1e4
han0ki <43035482+han0ki@users.noreply.github.com>
2019-03-23 14:24:13 +0300

    Add files via upload
    fixed

---
+++ Probeg2018-master/index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+
+
+	<meta charset="utf-8">
+	  <title><?php bloginfo('PROBEG2018')?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link href="<?= bloginfo('template_directory'); ?>/zerogrid.css?" rel="stylesheet" type="text/css" media="screen,projection" />
+   <link href="<?= bloginfo('template_directory'); ?>/style.css?" rel="stylesheet" type="text/css" media="screen,projection" />
+    <link href="<?= bloginfo('template_directory'); ?>/menu.css?" rel="stylesheet" type="text/css" media="screen,projection" />
+    <link href="<?= bloginfo('template_directory'); ?>/font-awesome/css/font-awesome.min.css?" rel="stylesheet" type="text/css" media="screen,projection" />
+    <link href="<?= bloginfo('template_directory'); ?>/owl-carousel/owl.carousel.css?" rel="stylesheet" type="text/css" media="screen,projection" />
+    <script type="text/javascript" src="<?php bloginfo("template_url"); ?>/js/jquery1111.min.js"></script>
+    <script type="text/javascript" src="<?php bloginfo("template_url"); ?>/js/script.js"></script>
+</head>
+ <?php get_header(); ?>
+<div class="home-pagee">
+	<div class="logosereja">	
+            <img src="<?php bloginfo("template_url") ?>/images/logo.jpg">
+			</div>
+	<div class="wrap-body">
+		<header class="">
+			<div class="logo">
+				<a href="#">Шумерлинский пробег</a>
+				<span>ТРАДИЦИОННЫЙ ЛЕГКОАТЛЕТИЧЕСКИЙ ПРОБЕГ ПОСВЯЩЕННЫЙ ДНЮ ГОРОДА ШУМЕРЛЯ</span>
+			</div>
+			<div id="cssmenu" class="align-center">
+				<ul>
+					<li class="active"><a href="#container"><span>Информация</span></a></li>
+					<li><a href="#container"><span>Положение</span></a></li>
+					<li><a href="#copyright"><span>Схема</span></a></li>
+					<li class="last"><a href="http://89.253.222.57/registr/reg.php?id=22200"><span>Регистрация</span></a></li>
+				</ul>
+			</div>
+			<div id="owl-slide" class="owl-carousel">
+				<div class="item">
+					<img src="<?php bloginfo("template_url") ?>/images/dsc_0011.jpg">
+				</div>
+				<div class="item">
+					<img src="<?php bloginfo("template_url") ?>/images/site.jpg">
+				</div>
+				<div class="item">
+					<img src="<?php bloginfo("template_url") ?>/images/NmHgQecTwt0.jpg">
+				</div>
+			</div>
+				</div>
+		</header>
+		<section id="container">
+			<div class="wrap-container">
+				<section class="content-box box-1">
+					<div class="zerogrid">
+						<div class="wrap-box">
+							<div class="box-header">
+								<h2>ВНИМАНИЕ!!!</h2>
+							</div>
+							<div class="box-content">
+								<p>ВНИМАНИЕ!!!
+ОТКРЫТА РЕГИСТРАЦИЯ НА
+ТРАДИЦИОННЫЙ ЛЕГКОАТЛЕТИЧЕСКИЙ ПРОБЕГ
+посвященный Дню города Шумерля
+15 сентября 2018 года в 10.00 стартует традиционный легкоатлетический пробег, посвященный Дню города Шумерля. В этом году маршрут пробега изменен. Пробег стартует, как и в прошло мгоду от Сквера Победы. Спортсмены выбегают по улице Ленина из города в сторону деревни Шумерля, поворачивают на объездную дорогу, пробегают по объездной дороге. Далее выполняю разворот в районе Лыжной базы и таким же путем возвращаются в город и финишируют на стадионе Труд. Дистанция 21,1 км (полумарафон). Ежегодно в нашем пробеге участвуют спортсмены и любители бега Шумерли, Чувашской Республики и Марий Эл В пробеге может участвовать каждый кто имеет соответствующую подготовку и имеет допуск врача Стартовый взнос не взымаетя. Участие в пробеге бесплатное.Предварительная заявка не требуется.</p>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="content-box box-style-1 box-2">
+					<div class="zerogrid">
+						<div class="wrap-box">
+							<div class="row">
+								<div class="col-1-3">
+									<div class="wrap-col">
+										<article>
+											<div class="post-thumbnail-wrap">
+												<a href="#" class="portfolio-box">
+													<img src="<?php bloginfo("template_url") ?>/images/1.jpg">
+													<div class="portfolio-box-second">
+														<img src="<?php bloginfo("template_url") ?>/images/1.jpg">
+													</div>
+												</a>
+											</div>
+											<div class="entry-header ">
+												<h3 class="entry-title">РУКОВОДСТВО</h3>
+												<div class="l-tags"><a href="#">Общее руководство по подготовке и проведению пробега осуществляет отдел ФКСиТ администрации г.Шумерля.Непосредственное проведение возлагается на МАОУДО "Детско-юношескую спортивную школу"(ДЮСШ),судейскую коллегию</a></div>
+											</div>
+										</article>
+									</div>
+								</div>
+								<div class="col-1-3">
+									<div class="wrap-col">
+										<article>
+											<div class="post-thumbnail-wrap">
+												<a href="#" class="portfolio-box">
+													<img src="<?php bloginfo("template_url") ?>/images/2.jpg">
+													<div class="portfolio-box-second">
+														<img src="<?php bloginfo("template_url") ?>/images/2.jpg">
+													</div>
+												</a>
+											</div>
+											<div class="entry-header ">
+												<h3 class="entry-title">ВРЕМЯ И МЕСТО ПРОВЕДЕНИЯ</h3>
+												<div class="l-tags"><a href="#">Пробег стартует со сквера Победы 15 сентября 2018 года в 10.00 час. Маршрут пробега: Сквер Победы (старт) - ул.Ленина-д.Шумерля-объездная дорога - Лыжная база -объездная дорога - д.Шумерля- ул.Ленина- Стадион "Труд"(финиш).Протяженность пробега - 21,1 км.</a></div>
+											</div>
+										</article>
+									</div>
+								</div>
+								<div class="col-1-3">
+									<div class="wrap-col">
+										<article>
+											<div class="post-thumbnail-wrap">
+												<a href="#" class="portfolio-box">
+													<img src="<?php bloginfo("template_url") ?>/images/3.jpg">
+													<div class="portfolio-box-second">
+														<img src="<?php bloginfo("template_url") ?>/images/3.jpg">
+													</div>
+												</a>
+											</div>
+											<div class="entry-header ">
+												<h3 class="entry-title">УЧАСТНИКИ, НАГРАЖДЕНИЕ</h3>
+												<div class="l-tags"><a href="#">В пробеге участвуют все желающие, имеющие допуск врача. Победители и призеры пробега определяются по следующим возрастным группам:- юноши - до 18 лет;- юниорки, женщины - 18 лет и старше;- юниоры,мужчины - 18 лет и старше;Победители и призеры в каждой возрастной группе награждаются призами и грамотами</a></div>
+											</div>
+										</article>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-1-3">
+									<div class="wrap-col">
+										<article>
+											<div class="post-thumbnail-wrap">
+												<a href="#" class="portfolio-box">
+													<img src="<?php bloginfo("template_url") ?>/images/4.jpg">
+													<div class="portfolio-box-second">
+														<img src="<?php bloginfo("template_url") ?>/images/4.jpg">
+													</div>
+												</a>
+											</div>
+											<div class="entry-header ">
+												<h3 class="entry-title">БЕЗОПАСНОСТЬ УЧАСТНИКОВ СОРЕВНОВАНИЙ</h3>
+												<div class="l-tags"><a href="#">За обеспечение безопасности участников ответсвенность несут организатор, главный судья соревнований, тренеры и представители команд.Пробег сопровождается машинами ГИБДД и скорой помощи.</a></div>
+											</div>
+										</article>
+									</div>
+								</div>
+								<div class="col-1-3">
+									<div class="wrap-col">
+										<article>
+											<div class="post-thumbnail-wrap">
+												<a href="#" class="portfolio-box">
+													<img src="<?php bloginfo("template_url") ?>/images/5.jpg">
+													<div class="portfolio-box-second">
+														<img src="<?php bloginfo("template_url") ?>/images/5.jpg">
+													</div>
+												</a>
+											</div>
+											<div class="entry-header ">
+												<h3 class="entry-title">ЗАЯВКИ</h3>
+												<div class="l-tags"><a href="#">Заявки, заверенные врачем, участники пробега подают в день старта в судейскую коллегию, которая состоится на стадионе "Труд" г.Шумерля в 9.30 час.</a></div>
+											</div>
+										</article>
+									</div>
+								</div>
+								<div class="col-1-3">
+									<div class="wrap-col">
+										<article>
+											<div class="post-thumbnail-wrap">
+												<a href="#" class="portfolio-box">
+														<img src="<?php bloginfo("template_url") ?>/images/6.jpg">
+													<div class="portfolio-box-second">
+														<img src="<?php bloginfo("template_url") ?>/images/6.jpg">
+													</div>
+												</a>
+											</div>
+											<div class="entry-header">
+												<h3 class="entry-title">ЦЕЛИ И ЗАДАЧИ</h3>
+												<div class="l-tags"><a href="#">Пробег проводится с целью популяризации оздоровительного бега среди населения г.шумерля, массового привлечения населения к систематическим занятиям физической культурой и спортом, пропаганды здорового образа жизни</a></div>
+											</div>
+										</article>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+				<section class="content-box box-3">																						
+																
+				</section>
+				
+		<footer>
+			<div class="zerogrid wrap-footer">
+				<div class="row">
+					<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;СХЕМА ЛЕГКОАТЛЕТИЧЕСКОГО ПРОБЕГА
+                                            ПОСВЯЩЕННОГО ДНЮ ГОРОДА ШУМЕРЛЯ</p>
+										<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A86b64105e8a2f37312c41586504dd08637e4955a943e070fd8606b4b3c95969f&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+
+				</div>
+			<div class="zerogrid bottom-footer">
+				<div class="row">
+					<div class="bottom-social">
+						<a href="https://instagram.com/danik_makarv"><i class="fa fa-instagram"></i></a>
+						<a href="https://instagram.com/sheluvsguess"><i class="fa fa-instagram"></i></a>
+					</div>
+				</div>
+				<div id="copyright">
+					Copyright @ - Designed by <a href="https://http://www.vandraren.ru/">VANDRAREN</a>
+				</div>
+				<div class="contact1">
+					<p>Официальный сайт традиционного лекгоатлетического пробега посвященный Дню города Шумерля</p>
+					
+				</div>
+				<div class="contact2">
+					<p>Контактная информация</p>
+				</div>
+				<div class="contact3">
+					<p>Электронная почта <span>sport@gshum.cap.ru </span></p>
+				</div>
+				<div class="contact4">
+					<p>Контактный телефон <span>8-835-362-05-69</span></p>
+				</div>
+
+			</div>
+		</footer>
+		<script type="text/javascript" src="<?php bloginfo("template_url"); ?>/owl-carousel/owl.carousel.js"></script>
+		<script>
+		$(document).ready(function() {
+		  $("#owl-slide").owlCarousel({
+			autoPlay: 6000,
+			items : 1,
+			itemsDesktop : [1199,1],
+			itemsDesktopSmall : [979,1],
+			itemsTablet : [768, 1],
+			itemsMobile : [479, 1],
+			navigation: true,
+			navigationText: ['<i class="fa fa-chevron-left fa-5x"></i>', '<i class="fa fa-chevron-right fa-5x"></i>'],
+			pagination: false
+		  });
+		});
+		</script>
+	</div>
+</body>
+</html>
